976179fdfecfc49f8dcb88d9e162063fccaa49ed
FraI3mega <projeapp2@gmail.com>
2024-02-19 16:46:53 +0100

    add continous mode

--- main.py
+++ main.py
@@ -5,24 +5,28 @@ from datetime import datetime
 import requests
 from prettytable import SINGLE_BORDER, PrettyTable
 
-przystanek = json.loads(
-    requests.get("https://dip.mzkopole.pl/getRealtime.json?stopPointSymbol=352").text
-)
-tablica = PrettyTable()
-tablica.set_style(SINGLE_BORDER)
-tablica.field_names = ["Nr", "Kierunek", "Czas"]
-# print(przystanek)
-for autobus in przystanek["departures"]:
-    departure_in = (
-        str(
-            (
-                datetime.fromtimestamp(autobus["realDeparture"] // 1000)
-                - datetime.fromtimestamp(time.time())
-            ).seconds
-            // 60
-        )
-        + " min"
+while True:
+    przystanek = json.loads(
+        requests.get(
+            "https://dip.mzkopole.pl/getRealtime.json?stopPointSymbol=263"
+        ).text
     )
-    # print(autobus["lineName"], autobus["directionName"], departure_in)
-    tablica.add_row([autobus["lineName"], autobus["directionName"], departure_in])
-print(tablica)
+    tablica = PrettyTable()
+    tablica.set_style(SINGLE_BORDER)
+    tablica.field_names = ["Nr", "Kierunek", "Odjazd"]
+    # print(przystanek)
+    for autobus in przystanek["departures"]:
+        departure_in = (
+            str(
+                (
+                    datetime.fromtimestamp(autobus["realDeparture"] // 1000)
+                    - datetime.fromtimestamp(time.time())
+                ).seconds
+                // 60
+            )
+            + " min"
+        )
+        # print(autobus["lineName"], autobus["directionName"], departure_in)
+        tablica.add_row([autobus["lineName"], autobus["directionName"], departure_in])
+    print(tablica)
+    time.sleep(60)
