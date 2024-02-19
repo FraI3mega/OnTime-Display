4316e3a0437ea45c89fe2abc350ba54244867b9d
FraI3mega <projeapp2@gmail.com>
2024-02-19 16:36:17 +0100

    add table display

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ import time
 from datetime import datetime
 
 import requests
+from prettytable import SINGLE_BORDER, PrettyTable
 
 przystanek = json.loads(
     requests.get("https://dip.mzkopole.pl/getRealtime.json?stopPointSymbol=352").text
 )
+tablica = PrettyTable()
+tablica.set_style(SINGLE_BORDER)
+tablica.field_names = ["Nr", "Kierunek", "Czas"]
 # print(przystanek)
 for autobus in przystanek["departures"]:
-    departure_in = datetime.fromtimestamp(
-        autobus["realDeparture"] // 1000
-    ) - datetime.fromtimestamp(time.time())
-    print(
-        autobus["lineName"], autobus["directionName"], departure_in.seconds // 60, "min"
+    departure_in = (
+        str(
+            (
+                datetime.fromtimestamp(autobus["realDeparture"] // 1000)
+                - datetime.fromtimestamp(time.time())
+            ).seconds
+            // 60
+        )
+        + " min"
     )
+    # print(autobus["lineName"], autobus["directionName"], departure_in)
+    tablica.add_row([autobus["lineName"], autobus["directionName"], departure_in])
+print(tablica)
